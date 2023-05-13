c0a03f0d4e73ff0a3cb06971b0913f34703d78f1
thayrov <thayrovg@gmail.com>
2023-05-13 12:18:32 -0600

    corrected functionality for 2nd challenge

--- DesafioEntregable2+ThayrovGarcia/desafio.js
+++ DesafioEntregable2+ThayrovGarcia/desafio.js
@@ -3,93 +3,142 @@ const fs = require('fs').promises;
 class ProductManager {
 	constructor(path) {
 		this.path = path;
+		this.products = [];
+		this.lastId = 0;
+	}
+
+	async initialize() {
+		try {
+			const data = await fs.readFile(this.path, 'utf-8');
+			if (data) {
+				this.products = JSON.parse(data);
+				this.lastId = this.products[this.products.length - 1].id;
+			}
+		} catch (error) {
+			console.error(`Error initializing Product Manager: ${error.message}`);
+		}
+	}
+
+	async save() {
+		try {
+			await fs.writeFile(this.path, JSON.stringify(this.products, null, 2));
+			console.log('Product Manager saved successfully');
+		} catch (error) {
+			console.error(`Error saving Product Manager: ${error.message}`);
+		}
 	}
 
 	async addProduct(product) {
-		const products = await this.getProducts();
-		const id = products.length > 0 ? products[products.length - 1].id + 1 : 1;
-		const newProduct = {...product, id};
-		products.push(newProduct);
-		await fs.writeFile(this.path, JSON.stringify(products, null, 2));
-		return newProduct;
+		if (
+			!product.title ||
+			!product.description ||
+			!product.price ||
+			!product.thumbnail ||
+			!product.code ||
+			!product.stock
+		) {
+			console.error('All fields are required');
+			return;
+		}
+
+		if (this.products.some(p => p.code === product.code)) {
+			console.error('Product with the same code already exists');
+			return;
+		}
+
+		this.lastId++;
+		product.id = this.lastId;
+		this.products.push(product);
+		await this.save();
+		console.log('Product added successfully');
 	}
 
 	async getProducts() {
-		try {
-			const data = await fs.readFile(this.path, 'utf-8');
-			return JSON.parse(data);
-		} catch (error) {
-			return [];
-		}
+		return this.products;
 	}
 
 	async getProductById(id) {
-		const products = await this.getProducts();
-		const product = products.find(p => p.id === id);
+		const product = this.products.find(p => p.id === id);
+
 		if (!product) {
-			throw new Error(`Product with id ${id} not found`);
+			console.error('Product not found');
+			return;
 		}
+
 		return product;
 	}
 
-	async updateProduct(id, productToUpdate) {
-		const products = await this.getProducts();
-		const productIndex = products.findIndex(p => p.id === id);
-		if (productIndex === -1) {
-			throw new Error(`Product with id ${id} not found`);
+	async updateProduct(id, updatedFields) {
+		const index = this.products.findIndex(p => p.id === id);
+
+		if (index === -1) {
+			console.error('Product not found');
+			return;
 		}
-		const updatedProduct = {...products[productIndex], ...productToUpdate};
-		products[productIndex] = updatedProduct;
-		await fs.writeFile(this.path, JSON.stringify(products, null, 2));
-		return updatedProduct;
+
+		this.products[index] = {...this.products[index], ...updatedFields};
+		await this.save();
+		console.log('Product updated successfully');
 	}
 
 	async deleteProduct(id) {
-		const products = await this.getProducts();
-		const productIndex = products.findIndex(p => p.id === id);
-		if (productIndex === -1) {
-			throw new Error(`Product with id ${id} not found`);
+		const index = this.products.findIndex(p => p.id === id);
+
+		if (index === -1) {
+			console.error('Product not found');
+			return;
 		}
-		const deletedProduct = products.splice(productIndex, 1)[0];
-		await fs.writeFile(this.path, JSON.stringify(products, null, 2));
-		return deletedProduct;
+
+		this.products.splice(index, 1);
+		await this.save();
+		console.log('Product deleted successfully');
 	}
 }
 
-const productManager = new ProductManager('./products.json');
+const manager = new ProductManager('./products.json');
+(async function () {
+	await manager.initialize();
 
-(async () => {
-	try {
-		let products = await productManager.getProducts();
-		console.log(products);
+	console.log(await manager.getProducts());
 
-		const newProduct = await productManager.addProduct({
-			title: 'producto prueba',
-			description: 'Este es un producto prueba',
-			price: 200,
-			thumbnail: 'Sin imagen',
-			code: 'abc123',
-			stock: 25,
-		});
-		console.log(newProduct);
+	await manager.addProduct({
+		title: 'producto prueba',
+		description: 'Este es un producto prueba',
+		price: 200,
+		thumbnail: 'Sin imagen',
+		code: 'abc123',
+		stock: 25,
+	});
 
-		products = await productManager.getProducts();
-		console.log(products);
+	console.log(await manager.getProducts());
 
-		const product = await productManager.getProductById(newProduct.id);
-		console.log(product);
+	await manager.addProduct({
+		title: 'producto repetido',
+		description: 'Este es un producto repetido',
+		price: 300,
+		thumbnail: 'Sin imagen',
+		code: 'abc123',
+		stock: 10,
+	});
 
-		const updatedProduct = await productManager.updateProduct(newProduct.id, {
-			title: 'producto actualizado',
-		});
-		console.log(updatedProduct);
+	await manager.addProduct({
+		title: 'producto nuevo',
+		description: 'Este es un producto nuevo',
+		price: 300,
+		thumbnail: 'Sin imagen',
+		code: 'def456',
+		stock: 10,
+	});
 
-		const deletedProduct = await productManager.deleteProduct(newProduct.id);
-		console.log(deletedProduct);
+	console.log(await manager.getProductById(2));
 
-		products = await productManager.getProducts();
-		console.log(products);
-	} catch (error) {
-		console.error(error);
-	}
+	console.log(await manager.getProductById(1));
+
+	await manager.updateProduct(2, {stock: 20});
+
+	console.log(await manager.getProductById(2));
+
+	await manager.deleteProduct(2);
+
+	console.log(await manager.getProducts());
 })();
