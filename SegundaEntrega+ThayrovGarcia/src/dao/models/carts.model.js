fd966b397e1bb0ced04a2d32b39ad4b914a9721f
thayrov <thayrovg@gmail.com>
2023-06-18 16:09:52 -0600

    refactor getCartById method on CartService

--- SegundaEntrega+ThayrovGarcia/src/dao/models/carts.model.js
+++ SegundaEntrega+ThayrovGarcia/src/dao/models/carts.model.js
@@ -9,6 +9,7 @@ const cartSchema = new Schema(
 				product: {
 					type: Schema.Types.ObjectId,
 					ref: 'Product',
+					required: true,
 				},
 				quantity: {
 					type: Number,
--- SegundaEntrega+ThayrovGarcia/src/services/carts.service.js
+++ SegundaEntrega+ThayrovGarcia/src/services/carts.service.js
@@ -16,7 +16,7 @@ export class CartService {
 			const populatedCart = await CartModel.findById(cartId)
 				.populate({
 					path: 'products.product',
-					select: 'title description price stock thumbnails status code',
+					select: 'title description code price stock thumbnails',
 					model: 'Product',
 				})
 				.exec();
@@ -26,39 +26,26 @@ export class CartService {
 			}
 
 			populatedCart.products = populatedCart.products.map(product => {
-				const {_id, quantity, product: {title} = {}} = product;
+				const {
+					_id,
+					quantity,
+					product: {title, description, code, price, stock, thumbnails} = {},
+				} = product;
 				return {
 					_id: _id.toString(),
 					quantity,
 					title: title || '',
+					description: description || '',
+					code: code || '',
+					price,
+					stock,
+					thumbnails,
 				};
 			});
 
 			console.log('Populated Cart:', populatedCart);
 
 			return populatedCart;
-		} catch (error) {
-			console.error(error);
-			throw error;
-		}
-	}
-
-	async addProductToCart(cartId, productId, quantity) {
-		try {
-			const cart = await CartModel.findById(cartId);
-			if (!cart) {
-				return null;
-			}
-			const existingProduct = cart.products.find(
-				item => item.product.toString() === productId,
-			);
-			if (existingProduct) {
-				existingProduct.quantity += quantity;
-			} else {
-				cart.products.push({product: productId, quantity: quantity});
-			}
-			const updatedCart = await cart.save();
-			return updatedCart;
 		} catch (error) {
 			console.error(error);
 			throw error;
@@ -73,19 +60,22 @@ export class CartService {
 				throw new Error('Product not found');
 			}
 
-			console.log(productToAdd._id);
-
 			let cart = await CartModel.findOneAndUpdate(
 				{_id: cartId, 'products.product': productToAdd._id},
 				{
 					$inc: {'products.$.quantity': 1},
 				},
+				{new: true},
 			);
 
 			if (!cart) {
-				cart = await CartModel.findByIdAndUpdate(cartId, {
-					$push: {products: {product: productToAdd._id, quantity: 1}},
-				});
+				cart = await CartModel.findByIdAndUpdate(
+					cartId,
+					{
+						$push: {products: {product: productToAdd._id, quantity: 1}},
+					},
+					{new: true},
+				);
 			}
 
 			return cart;
