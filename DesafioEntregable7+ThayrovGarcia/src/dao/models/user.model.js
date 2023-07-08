29c56476c77dc2f3fa271bf11bf9ddcb1c33b500
thayrov <thayrovg@gmail.com>
2023-07-08 16:42:19 -0600

    modify userModel to use bcrypt

--- DesafioEntregable7+ThayrovGarcia/src/dao/models/user.model.js
+++ DesafioEntregable7+ThayrovGarcia/src/dao/models/user.model.js
@@ -1,5 +1,7 @@
 import {Schema, model} from 'mongoose';
 
+import {createHash} from '../utils.js';
+
 const UserSchema = new Schema({
 	first_name: {type: String, required: true},
 	last_name: {type: String, required: true},
@@ -7,6 +9,11 @@ const UserSchema = new Schema({
 	age: {type: Number, required: true},
 	password: {type: String, required: true},
 	role: {type: String, default: 'user'},
+});
+
+UserSchema.pre('save', function (next) {
+	this.password = createHash(this.password);
+	next();
 });
 
 const UserModel = model('User', UserSchema);
