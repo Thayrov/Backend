bd950308c6e853f5af8be25319a818e12e1869f0
thayrov <thayrovg@gmail.com>
2023-07-08 13:08:02 -0600

    modify .gitignore

--- DesafioEntregable6+ThayrovGarcia/src/routes/messages.routes.js
+++
@@ -1,8 +0,0 @@
-import * as messagesController from '../controllers/messages.controller.js';
-
-import express from 'express';
-
-export const routerMessages = express.Router();
-
-routerMessages.get('/', messagesController.getAllMessages);
-routerMessages.post('/', messagesController.createMessage);
