6084e2f3d13ab88cfe871ff9675e32e7247c088b
thayrov <thayrovg@gmail.com>
2023-07-09 16:17:04 -0600

    add unique parameter to userModel email

--- DesafioEntregable7+ThayrovGarcia/src/dao/models/user.model.js
+++ DesafioEntregable7+ThayrovGarcia/src/dao/models/user.model.js
@@ -5,7 +5,7 @@ import {createHash} from '../../config/bcrypt.js';
 const UserSchema = new Schema({
 	first_name: {type: String, required: true},
 	last_name: {type: String, required: true},
-	email: {type: String, required: true},
+	email: {type: String, required: true, unique: true},
 	age: {type: Number, required: true},
 	password: {type: String, required: true},
 	role: {type: String, default: 'user'},
